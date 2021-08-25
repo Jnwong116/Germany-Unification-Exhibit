52f78d29a9250e3901914c6c86ff7871d67bfba8
Jordan Wong <jnwong116@gmail.com>
2021-08-25 15:44:16 -0400

    Finished map updating features and added other pages for each source

--- index_script.js
+++ index_script.js
@@ -4,10 +4,25 @@ const searchButton = document.getElementById('searchButton');
 const map = document.getElementById('map');
 const berlin = document.getElementById('berlin');
 const koniggratz = document.getElementById('koniggratz');
+const prague = document.getElementById('prague');
+const anhalt = document.getElementById('anhalt');
+const france = document.getElementById('france');
+const bavaria = document.getElementById('bavaria');
+const austria = document.getElementById('austria');
+
+const berlin_preview = document.getElementById('berlin_preview');
+const koniggratz_preview = document.getElementById('koniggratz_preview');
+const prague_preview = document.getElementById('prague_preview');
+const anhalt_preview = document.getElementById('anhalt_preview');
+const france_preview = document.getElementById('france_preview');
+const bavaria_preview = document.getElementById('bavaria_preview');
+const austria_preview = document.getElementById('austria_preview');
+
+var map_pins = [berlin, koniggratz, prague, anhalt, france, bavaria, austria];
 
 const dateDisplay = document.getElementById('dateDisplay');
 
-var dates = ["April, 1861", "April, 1866", "May, 1866", "July 3, 1866", "July 5, 1866", "December, 1866"];
+var dates = ["April, 1861", "April, 1866", "May, 1866", "June 3, 1866", "July 3, 1866", "July 5, 1866", "September, 1866", "December, 1866", "April 11, 1868"];
 var dateIndex = 0;
 
 function autocomplete(inp, arr, button) {
@@ -163,14 +178,190 @@ function dateChange(direction) {
         } 
     }
 
-    if (dateIndex > 4) {
+    if (dateIndex > 5) {
         map.src = 'Germany_Map_After.png';
     }
 
-    if (dateIndex <= 4) {
+    if (dateIndex <= 5) {
         map.src = 'German Map Before.svg';
+    }
+
+    updateMap();
+}
+
+function updateMap() {
+    clearMap();
+
+    var map_pin;
+
+    switch(dateIndex) {
+        case 0:
+            map_pin = berlin;
+            break;
+        case 1:
+            map_pin = berlin;
+            break;
+        case 2: 
+            map_pin = prague;
+            break;
+        case 3:
+            map_pin = berlin;
+            break;
+        case 4:
+            map_pin = koniggratz;
+            break;
+        case 5:
+            map_pin = anhalt;
+            break;
+        case 6:
+            map_pin = france;
+            break;
+        case 7:
+            map_pin = bavaria;
+            break;
+        case 8: 
+            map_pin = austria;
+            break;
+    }
+
+    map_pin.style.display = "block";
+    map_pin.scrollIntoView({block: "center"});
+
+    showPreview();
+}
+
+function clearMap() {
+    for (var i = 0; i < map_pins.length; i++) {
+        map_pins[i].style.display = "none";
+    }
+}
+
+function showPreview() {
+    switch(dateIndex) {
+        case 0:
+            berlin_preview.childNodes[1].src = './sources/Prussian-needle-rifle.jpg';    
+        
+            berlin_preview.childNodes[3].innerHTML = "In 1861, Helmuth von Moltke, the Chief of the Prussian General Staff reviewed Prussia's military improvements, particularly \
+            the new needle gun. He evaluated how it would change the way the Prussian army fought and how it would give the Prussian military an advantage over neighboring\
+            countries, particularly France. He takes note of the fact none of their neighboring powers have brought their firearms up to the same level and how this new technology \
+            will bring Prussia great success in the event of a war breaking out.";
+            
+            berlin.onclick = function() {
+                location.href = 'moltke_gun_improvements/moltke_gun_doc.html';
+            }
+            break;
+        case 1:
+            berlin_preview.childNodes[1].src = './sources/prussia-railways.jpg';
+            
+            berlin_preview.childNodes[3].innerHTML = "In 1866, with the possibility of war between Prussia and Austria rising up, Helmuth von Moltke began to formulate a strategy \
+            for mobilizing Prussian troops and to win the war. With Prussia’s disadvantage in terms of numbers, Moltke had to find a way to overcome this disadvantage. The \
+            solution he comes up with is to utilize Prussia’s railroads. By doing so, he notes that Prussia can have their army on the front-lines much faster than Austria and \
+            its potential allies as they do not have as many railroads. By getting troops to the fight faster than Austria and its allies, Prussia will encounter a much smaller \
+            army and will remove the advantage of numbers.";
+
+            berlin.onclick = function() {
+                location.href = '';
+            }
+
+            berlin_preview.style.height = '500px';
+            break;
+        case 2:
+            prague_preview.childNodes[1].src = './sources/austrian-propaganda.png';
+
+            prague_preview.childNodes[3].innerHTML = "In May of 1866, this cartoon was printed in a Czech magazine. The title says: “What would these two give for having eyes \
+            in the back?”. In the cartoon there are two boats, one is Prussia and the other is Italy. Prussia has Schleswig-Holstein in its boat and Italy has Venice. Both boats \
+            are going backwards down the waterfall of war. At the bottom of the waterfall there is a rock which is the united forces of Austrian nations. ";
+
+            prague_preview.onclick = function() {
+                location.href = '';
+            }
+
+            prague_preview.style.height = '450px';
+            break;
+        case 3:
+            berlin_preview.childNodes[1].src = './sources/prussian-propaganda.jpg';
+            
+            berlin_preview.childNodes[3].innerHTML = "On June 3, just two weeks before the outbreak of the war, Wilhelm Scholz published this cartoon in a Prussian satirical \
+            journal called Kladderadatsch. The caption reads: “The opportunity is favorable, either to become great and respond to the wishes of the Prussian people, or to become \
+            the most popular man in Germany by acceding to their general wishes.” The left hand side shows a detailed plan of how Bismarck can respond to the wishes of Prussia \
+            through reform, and the other side shows Bismarck covering his ears to chants demanding he resign.";
+
+            berlin.onclick = function() {
+                location.href = '';
+            }
+
+            berlin_preview.style.height = '450px';
+            break;
+        case 4:
+            koniggratz_preview.childNodes[1].src = './sources/battle-of-koniggratz.jpg';
+
+            koniggratz_preview.childNodes[3].innerHTML = "On July 3, 1866, the Battle of Koniggratz took place. This was the decisive battle in the Austro-Prussian war. \
+            The image above depicts the battle between the Austrian and Prussian army. You can see the chaos of the battle and how the Austrian forces appear as though they are \
+            making their last stand.";
+
+            koniggratz_preview.onclick = function() {
+                location.href = '';
+            }
+
+            break;
+        case 5:
+            anhalt_preview.childNodes[1].src = '';
+
+            anhalt_preview.childNodes[3].innerHTML = "Two days after the Battle of Koniggratz, on July 5, 1866, Wilhelm von Kugelgen wrote this letter, reflecting on the \
+            war and how the people of Prussia had reacted to news of the battle. He speaks about the pain families feel after losing their children and his hopes for the future \
+            of Germany under Bismarck.";
+
+            anhalt_preview.onclick = function() {
+                location.href = '';
+            }
+
+
+            anhalt_preview.style.height = '200px';
+            break;
+        case 6:
+            france_preview.childNodes[1].src = './sources/france-cartoon.jpg';
+
+            france_preview.childNodes[3].innerHTML = "In September, 1866, a caricature appeared in a French journal Le Charivari. It shows a Prussian soldier trying to \
+            stitch together the defeated German states: Saxony, Hanover, Nassau, and Frankfurt; together with Prussia. The caption says: “It’s one thing to know how to use a \
+            needle … But it’s a skill that should not be abused.”";
+
+            france_preview.onclick = function() {
+                location.href = '';
+            }
+
+
+            france_preview.style.height = '500px';
+            break;
+        case 7:
+            bavaria_preview.childNodes[1].src = '';
+
+            bavaria_preview.childNodes[3].innerHTML = "After Prussia’s victory, many states were forced into the North German Confederation. The southern German states still \
+            remained independent however. In December of 1866, Sir Henry Francis Howard wrote a report to the British Foreign Office on the mood in the annexed territories and \
+            southern states. Sir Henry was the British envoy to Bavaria from 1866 to 1872.";
+
+            bavaria_preview.onclick = function() {
+                location.href = '';
+            }
+
+
+            bavaria_preview.style.height = '200px';
+            break;
+        case 8:
+            austria_preview.childNodes[1].src = './sources/southern-german-states-cartoon.jpg';
+
+            austria_preview.childNodes[3].innerHTML = "Two years after the war, in April 1868, this woodcut appeared in an Austrian journal Figaro. It shows Prussia whipping \
+            the other Germans into the North German Confederation. The caption reads, “In the end, the South Germans, too, will join; we are obviously still too liberal for them!”";
+
+            austria_preview.onclick = function() {
+                location.href = '';
+            }
+
+
+            austria_preview.style.height = '300px';
+            break;
     }
 }
 
 autocomplete(form, dates, searchButton);
 dateDisplay.innerHTML = dates[dateIndex];
+updateMap();
